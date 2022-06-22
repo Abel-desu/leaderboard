b52bdafc6ea799b890cae8855f68f1e2267b0410
Abel Desalegn <95746961+Abel-desu@users.noreply.github.com>
2022-06-22 16:03:33 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import RecentScores from './RecentScore'
-import './style.css';
-const show = (name, score) => {
-    document.querySelector('#list').innerHTML += `<ul>
-      <li>${name}:${score}</li></ul>`;
-  };
-  RecentScores.forEach((score) => {
-    show(score.name, score.score);
-  });
