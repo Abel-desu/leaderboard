3026980ee6681100e2706c26caf465f4ed4d1c06
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-06-23 03:28:12 +0300

    fix error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import RecentScores from './RecentScore'
+import RecentScores from './RecentScore';
 import './style.css';
+
 const show = (name, score) => {
-    document.querySelector('#list').innerHTML += `<ul>
+  document.querySelector('#list').innerHTML += `<ul>
       <li>${name}:${score}</li></ul>`;
-  };
-  RecentScores.forEach((score) => {
-    show(score.name, score.score);
-  });
+};
+RecentScores.forEach((score) => {
+  show(score.name, score.score);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+  mode: 'development',
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-    title: 'Output Management',
-     template: './src/index.html',
+      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   output: {
